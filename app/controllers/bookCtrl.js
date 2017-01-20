52b1961c40f1d679de4a9497b45f4968ac9ba66e
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-20 09:49:20 -0600

    moved factory to it's own js file

--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -7,17 +7,3 @@ app.controller("BookCtrl", function($scope, guideFactory) {
 
 
 }) //end of controller
-
-.factory("guideFactory", ($http) => {
-  return {
-    getGuides : () => {
-      return $http
-      .get("data/guides.json")
-      .then((response)=> {
-        console.log(response.data.guides)
-        return response.data.guides
-      }) // end of then that parses data to an array of guides called guides
-    } // end of getGuides function
-  } // end of object
-
-})
